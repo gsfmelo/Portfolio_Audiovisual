541f688600d90f77d91b9258b6dc89865d9422ac
Geovanna Melo <gsfmelo@gmail.com>
2026-07-29 23:17:50 -0300

    decimo commit

--- src/app/data/projetos.js
+++ src/app/data/projetos.js
@@ -18,10 +18,10 @@ export const projetosDestaque = [
         meta2: "2024–26",
         tituloNormal: "Quando o",
         tituloItalico: "Telefone Tocar",
-        sinopse: "Curta-Metragem aprovado pela Lei Paulo Gustavo Olinda 2023. Atualmente em fase de Pós-Produção.",
+        sinopse: <>Sinopse: Cláudia é uma mulher ansiosa e qualquer coisa é capaz de levar aos seus limites. Após receber uma ligação inesperada com um convite para se encontrar com uma amiga próxima, ela teme que, talvez, um segredo seu tenha sido descoberto. A amiga, por outro lado, só precisava de um motivo para tirar Cláudia de casa. <br /><br />Curta-Metragem aprovado pela Lei Paulo Gustavo Olinda 2023.</>,
         funcoes: "direção, roteiro e produção",
-        link: "https://drive.google.com/file/d/1UwLpbZf70DCDfil8XUVGNMMuXqQ16A50/view?usp=sharing",
-        imagem: ""
+        link: "https://youtu.be/cHRQXVLH-w8",
+        imagem: "/telefone1.png"
     }
 ];
 
@@ -57,10 +57,10 @@ export const projetosAutoral = [
         meta2: "2024",
         tituloNormal: "Beijos e",
         tituloItalico: "Confidências",
-        sinopse: "Minidocumentário desenvolvido inteiramente por pesquisa autoral com o incentivo da bolsa BICC-UFPE. Exibido na Mostra BICC em Maio de 2025.",
+        sinopse: <>Sinopse: "beijos e confidências” é um íntimo mergulho nas experiências pessoais da narradora ao longo da descoberta de sua sexualidade. Reflete a importância da representatividade LGBTQIAPN+ no audiovisual por meio de retratos de beijos icônicos nas telonas, revelando como essas cenas impactam vidas e inspiram pessoas que trilharam um longo caminho em busca da aceitação e celebração de sua identidade.<br /><br />Minidocumentário desenvolvido por pesquisa autoral com o incentivo da bolsa BICC-UFPE.</>,
         funcoes: "direção, roteiro, produção e montagem",
         link: "https://drive.google.com/file/d/1NVSyJTB8mDjNUbI-60QpmalkpgMbrOLy/view",
-        imagem: ""
+        imagem: "/beijos1.png"
     },
     {
         id: 6,
@@ -68,10 +68,10 @@ export const projetosAutoral = [
         meta2: "2019–21",
         tituloNormal: "Insônia",
         tituloItalico: "",
-        sinopse: "Meu primeiro curta-metragem universitário dirigido e finalizado por mim.",
+        sinopse: <>Sinopse: Márcia passa mais uma noite em claro tentando vencer a insônia. À medida que o cansaço se acumula, sons inexplicáveis, a escuridão da casa e acontecimentos cada vez mais inquietantes fazem a fronteira entre realidade e imaginação se tornar incerta. Enquanto espera o amanhecer, ela precisa enfrentar os próprios medos e as consequências de uma mente à beira do colapso.<br /><br />Curta-Metragem realizado para a disciplina de Direção. </>,
         funcoes: "direção, assist. de montagem e finalização",
         link: "https://drive.google.com/file/d/1sACciKtBloNwgcr3t-hCfFly3PED6IE-/view",
-        imagem: ""
+        imagem: "/insonia1.jpeg"
     },
     {
         id: 7,
@@ -79,7 +79,7 @@ export const projetosAutoral = [
         meta2: "2021",
         tituloNormal: "All About",
         tituloItalico: "Love",
-        sinopse: "Vídeo-ensaio universitário sobre música e cinema e trabalho final de disciplina de Cinema Musical.",
+        sinopse: "Vídeo-ensaio realizado para a disciplina de Cinema Musical no semestre 2021.1. Trabalho que me possibilitou aprender edição/montagem de vídeos.",
         funcoes: "montagem",
         link: "https://drive.google.com/file/d/1sACciKtBloNwgcr3t-hCfFly3PED6IE-/view",
         imagem: ""
@@ -90,7 +90,7 @@ export const projetosAutoral = [
         meta2: "2019",
         tituloNormal: "Olívia",
         tituloItalico: "",
-        sinopse: "Projeto de Instasérie universitário realizado como trabalho final da disciplina de Dispositivos Móveis no Audiovisual.",
+        sinopse: <>Para dar vida a garota dos seus sonhos, Júlia cria um Instagram fictício, chamado Olívia. Um jogo de manipulação começa quando Júlia se sente ameaçada por Letícia, uma caloura que rouba a identidade exposta no perfil. <br /><br />Instasérie desenvolvida para a disciplina de Dispositivos Móveis no Audiovisual.</>,
         funcoes: "direção, roteiro, produção e fotografia",
         link: "https://www.instagram.com/olivia_serie/",
         imagem: ""
@@ -98,37 +98,59 @@ export const projetosAutoral = [
 ];
 
 export const projetosColaboracoes = [
-    {
+        {
         id: 9,
+        meta1: "CURTA · 21'",
+        meta2: "2026",
+        tituloNormal: "Casa de ",
+        tituloItalico: "Vó",
+        sinopse: <>Sinopse: Após a morte de sua avó, o jovem Gabriel enfrenta um fim de ano melancólico, marcado pela nova e fragmentada dinâmica familiar. O curta mergulha na jornada emocional do neto, que, em meio à dor da perda, revisita as memórias afetivas e as tradições compartilhadas com a avó em sua casa de praia. <br /><br />Curta-Metragem aprovado pela Lei Paulo Gustavo Paulista 2024.</>,
+        funcoes: "assistência de direção",
+        link: "",
+        imagem: ""
+    },
+        {
+        id: 10,
+        meta1: "CURTA · 6'",
+        meta2: "2025",
+        tituloNormal: "Declaração de",
+        tituloItalico: "Amor",
+        sinopse: <>Sinopse: “Para me amar, confesso que já tentei de tudo.” O projeto de curta-metragem possui formato de doc-ficção, em uma narrativa sensível sobre crescimento e amor próprio.<br /><br />Curta-Metragem aprovado pela Lei Paulo Gustavo Paulista 2024.</>,
+        funcoes: "assistência de direção e montagem",
+        link: "https://drive.google.com/file/d/1tliO9UMUqJRinbjKLdZsttldngD8EEzv/view",
+        imagem: "/declaracao1.png"
+    },
+    {
+        id: 11,
         meta1: "CURTA · 21'",
         meta2: "2024",
         tituloNormal: "2000.com",
         tituloItalico: "",
-        sinopse: "Curta-Metragem dirigido por Eduardo Gomes. Atualmente em fase de Pós-Produção.",
+        sinopse: <>Sinopse: O que era para ser uma festa se transforma em um pesadelo quando o sumiço de seus amigos revela a Ju e Rafa um grupo de agressores vindos da internet. Enquanto tentam sobreviver ao ataque, uma colega aparece para ajudar, transformando a noite de festa em um banho de sangue.  <br /><br />Curta-Metragem universitário realizado como Trabalho de Conclusão de Curso do diretor Eduardo Gomes.</>,
         funcoes: "assistência de direção e montagem",
         link: "https://drive.google.com/file/d/1tliO9UMUqJRinbjKLdZsttldngD8EEzv/view",
         imagem: ""
     },
     {
-        id: 10,
+        id: 12,
         meta1: "CURTA · 6'",
         meta2: "2022",
         tituloNormal: "ART",
         tituloItalico: "",
-        sinopse: "Curta-Metragem dirigido por Pietra Couto com aclamação internacional.",
+        sinopse: <>Sinopse: O curta conta a história de Nous, um jovem de 22 anos que está tentando se encontrar. Art é sobre se perder, sobre se enxergar e se entender: ninguém nasce sabendo quem é, e ninguém é o mesmo para sempre. Está tudo bem em se sentir perdido, e está tudo bem em se conhecer e se reconhecer quantas vezes for preciso. <br /><br />Curta-Metragem realizado com apoio da Lei Aldir Blanc em 2022.</>,
         funcoes: "assistência de direção e montadora",
         link: "https://www.youtube.com/watch?v=x9Y2MaDu2hQ&ab_channel=ART%3ACurta-metragem",
-        imagem: ""
+        imagem: "/art1.png"
     },
     {
-        id: 11,
-        meta1: "CURTA · 16'",
-        meta2: "2019",
-        tituloNormal: "Amar é",
-        tituloItalico: "Reticência",
-        sinopse: "Curta-metragem universitário dirigido por Diá e trabalho final da disciplina de Produção.",
-        funcoes: "assistência de fotografia",
-        link: "https://www.youtube.com/watch?v=GEVJOEe4gA&ab_channel=ZetyYouruchi",
-        imagem: ""
+    id: 13,
+    meta1: "CURTA · 16'",
+    meta2: "2019",
+    tituloNormal: "Amar é",
+    tituloItalico: "Reticência",
+    sinopse: <>Sinopse: Após mais um dia de trabalho no cemitério, Seu Zuza volta para uma casa tomada pelo silêncio. Em meio a memórias que insistem em permanecer, ele enfrenta a solidão e a ausência de quem já não está, revelando um delicado retrato sobre o luto e a passagem do tempo.<br /><br />Curta-Metragem realizado para a disciplina de Produção e primeiro trabalho audiovisual universitário. </>,
+    funcoes: "assistência de fotografia",
+    link: "https://www.youtube.com/watch?v=NVkbBxgPe9k",
+    imagem: ""
     }
 ];
